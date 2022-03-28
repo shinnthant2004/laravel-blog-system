fd7f77ed470037a58d55c837ceca8c091d8fb6c1
shinthantequi@gmail.com <shinthantequi@gmail.com>
2022-03-29 05:37:27 +0630

    added search feature

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,10 +1,27 @@
 <x-layout>
 
-    <x-slot name="title">
-        <title>{{ $blog->title }}</title>
-    </x-slot>
+    <div class="container">
+        <div class="row">
+          <div class="col-md-6 mx-auto text-center">
+            <img
+              src="https://creativecoder.s3.ap-southeast-1.amazonaws.com/blogs/GOLwpsybfhxH0DW8O6tRvpm4jCR6MZvDtGOFgjq0.jpg"
+              class="card-img-top"
+              alt="..."
+            />
+            <h3 class="my-3">{{ $blog->title }}</h3>
+            <div>
+                <div>{{ $blog->author->name }}</div>
+                <div class="badge bg-primary">{{ $blog->category->name }}</div>
+                <div class="text-secondary">{{ $blog->created_at->diffForHumans() }}</div>
+            </div>
+            <p class="lh-md mt-3">
+              {{ $blog->body }}
+            </p>
+          </div>
+        </div>
+      </div>
 
- <h1>{{ $blog->title }}</h1>
- <p>{!! $blog->body !!}</p>
- <a href="/">back</a>
+      <x-subscribe></x-subscribe>
+      <x-blogs_you_may_like_section :randomBlogs="$randomBlogs"></x-blogs_you_may_like_section>
+
 </x-layout>
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,21 +1,10 @@
 <x-layout>
 
-<x-slot name="title">
-    <title>Blogs</title>
-</x-slot>
+   <x-hero/>
 
-@foreach ($blogs as $blog )
-<div class="">
+  <x-blogs-section :blogs="$blogs" :categories="$categories" :currentCategory="$currentCategory ?? null"/>
 
-    <h1><a href="blogs/{{ $blog->slug }}">{{ $blog->title }}</a></h1>
-     <h3>Author <a href="users/{{ $blog->author->username }}">{{ $blog->author->name }}</a></h3>
- <div>
-    <p>Published by {{ $blog->created_at->diffForHumans() }}</p>
-    <a href="categories/{{ $blog->category->slug }}">{{ $blog->category->name }}</a>
-    <p>{{ $blog->body }}</p>
- </div>
-
-</div>
-@endforeach
+  <x-subscribe/>
 
 </x-layout>
+
